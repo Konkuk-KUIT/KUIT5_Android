13db633fb963dd8ca9053959224b584da632ef64
adagioCHAN <gguakchan@gmail.com>
2025-04-02 16:04:50 +0900

    자산 탭 폰트 미적용, 초록 글자 미적용

---
+++ app/src/main/java/com/kuit/kuit5/model/HomeContentData().kt
@@ -0,0 +1,8 @@
+package com.kuit.kuit5.model
+
+data class HomeContentData(
+    val icon: Int,
+    val value: Int,
+    var title: String,
+    var changeValue : Int
+)
---
+++ app/src/main/java/com/kuit/kuit5/ui/home/component/HomeContentItem.kt
@@ -0,0 +1,83 @@
+package com.kuit.kuit5.ui.home.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.kuit.kuit5.R
+import com.kuit.kuit5.model.AssetData
+import com.kuit.kuit5.model.HomeContentData
+import com.kuit.kuit5.ui.theme.BankSaladTheme.colors
+import com.kuit.kuit5.ui.theme.BankSaladTheme.typography
+import com.kuit.kuit5.util.toDecimalFormat
+
+@Composable
+fun HomeContentItem(
+    content: HomeContentData,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 12.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ){
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Icon(
+                modifier = Modifier
+                    .size(30.dp),
+                painter = painterResource(id = content.icon),
+                contentDescription = "asset icon",
+                tint = Color.Unspecified
+            )
+            Column(
+                modifier = Modifier.padding(start = 10.dp)
+            ){
+                Text(
+                    text = content.value.toDecimalFormat()+"원",
+                    color = colors.gray400,
+                    style = typography.body_01_M_14
+                )
+                Spacer(modifier = Modifier.size(4.dp))
+                Text(
+                    text = content.title,
+                    color = colors.black,
+                    style = typography.body_01_M_14
+                )
+            }
+        }
+        Row(verticalAlignment = Alignment.CenterVertically){
+            Image(
+                painter = painterResource(id = R.drawable.img_bluetriangle_icon),
+                contentDescription = "greencash icon",
+                modifier = Modifier
+                    .size(width = 16.dp, height = 10.dp)
+            )
+            Text(
+                text = content.changeValue.toDecimalFormat()+"원",
+                color = colors.blue,
+                style = typography.body_01_M_14
+            )
+            Icon(
+                painter = painterResource(id = R.drawable.ic_assets_right_arrow),
+                contentDescription = "right arrow",
+            )
+        }
+    }
+}
